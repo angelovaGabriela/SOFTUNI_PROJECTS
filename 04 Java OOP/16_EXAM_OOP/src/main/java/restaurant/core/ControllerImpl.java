7a8a6c1d3835b0fae7401b1b0d9c69695b2a4c2b
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-04-14 23:18:23 +0200

    Added: new changes in problem solution from JAVA OOP course

--- 04 Java OOP/16_EXAM_OOP/src/main/java/restaurant/core/ControllerImpl.java
+++ 04 Java OOP/16_EXAM_OOP/src/main/java/restaurant/core/ControllerImpl.java
@@ -1,34 +1,86 @@
 package restaurant.core;
 
+import restaurant.common.ExceptionMessages;
+import restaurant.common.OutputMessages;
 import restaurant.core.interfaces.Controller;
+import restaurant.entities.drinks.Fresh;
+import restaurant.entities.drinks.Smoothie;
+import restaurant.entities.healthyFoods.Salad;
+import restaurant.entities.healthyFoods.VeganBiscuits;
 import restaurant.entities.healthyFoods.interfaces.HealthyFood;
 import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.entities.tables.InGarden;
+import restaurant.entities.tables.Indoors;
 import restaurant.entities.tables.interfaces.Table;
 import restaurant.repositories.interfaces.*;
+
+import static restaurant.common.OutputMessages.FOOD_ADDED;
 
 public class ControllerImpl implements Controller {
 
 
-    public ControllerImpl(HealthFoodRepository<HealthyFood> healthFoodRepository, BeverageRepository<Beverages> beverageRepository, TableRepository<Table> tableRepository) {
+    private final HealthFoodRepository<HealthyFood> healthFoodRepository;
+    private final BeverageRepository<Beverages> beverageRepository;
+    private final TableRepository<Table> tableRepository;
 
+    public ControllerImpl(HealthFoodRepository<HealthyFood> healthFoodRepository,
+                          BeverageRepository<Beverages> beverageRepository,
+                          TableRepository<Table> tableRepository) {
+        this.healthFoodRepository = healthFoodRepository;
+        this.beverageRepository = beverageRepository;
+        this.tableRepository = tableRepository;
     }
 
     @Override
     public String addHealthyFood(String type, double price, String name) {
-        //TODO:
-        return null;
+        HealthyFood food = type.equals("Salad")
+                ? new Salad(name, price)
+                : new VeganBiscuits(name,price);
+
+        HealthyFood previouslyAddedFood = this.healthFoodRepository.foodByName(name);
+        if (previouslyAddedFood == null) { // if the repository doesn't contain this food == null
+            this.healthFoodRepository.add(food);
+            return String.format(OutputMessages.FOOD_ADDED, name);
+
+        } else {
+            throw new IllegalArgumentException(String.format(ExceptionMessages.FOOD_EXIST, name));
+        }
+
+
     }
 
     @Override
     public String addBeverage(String type, int counter, String brand, String name){
-        //TODO:
-        return null;
+        Beverages beverage = type.equals("Fresh")
+                ? new Fresh(name,counter, brand)
+                : new Smoothie(name,counter,brand);
+
+        Beverages previousAddedBeverage = this.beverageRepository.beverageByName(name, brand);
+        if (previousAddedBeverage == null) { // if the repository doesn't contain this food == null
+            this.beverageRepository.add(beverage);
+            return String.format(OutputMessages.BEVERAGE_ADDED, type, brand);
+
+        } else {
+            throw new IllegalArgumentException(String.format(ExceptionMessages.BEVERAGE_EXIST, name));
+        }
+
+
     }
 
     @Override
     public String addTable(String type, int tableNumber, int capacity) {
-        //TODO:
-        return null;
+        Table table = type.equals("Indoors")
+                ? new Indoors(tableNumber, capacity)
+                : new InGarden(tableNumber, capacity);
+
+        Table previousAddedTable = this.tableRepository.byNumber(tableNumber);
+        if (previousAddedTable == null) { // if the repository doesn't contain this food == null
+            this.tableRepository.add(table);
+            return String.format(OutputMessages.TABLE_ADDED, tableNumber);
+
+        } else {
+            throw new IllegalArgumentException(String.format(ExceptionMessages.TABLE_IS_ALREADY_ADDED, tableNumber));
+        }
     }
 
     @Override
--- 04 Java OOP/16_EXAM_OOP/src/main/java/restaurant/entities/healthyFoods/VeganBiscuits.java
+++ 04 Java OOP/16_EXAM_OOP/src/main/java/restaurant/entities/healthyFoods/VeganBiscuits.java
@@ -3,7 +3,7 @@ package restaurant.entities.healthyFoods;
 public class VeganBiscuits extends Food {
     private static final int InitialVeganBiscuitPortion =  205;
 
-    public VeganBiscuits(String name, double portion, double price) {
+    public VeganBiscuits(String name, double price) {
         super(name, InitialVeganBiscuitPortion, price);
     }
 }
