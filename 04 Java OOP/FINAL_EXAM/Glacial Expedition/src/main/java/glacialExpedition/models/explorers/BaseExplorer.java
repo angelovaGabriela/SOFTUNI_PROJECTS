fe0cdeb4d15ba03ab43a9731f0b453a280cfc6e3
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-04-25 21:31:54 +0200

    Added: New project from Java OOP courses / Glacial Expedition

---
+++ 04 Java OOP/FINAL_EXAM/Glacial Expedition/src/main/java/glacialExpedition/models/explorers/BaseExplorer.java
@@ -0,0 +1,66 @@
+package glacialExpedition.models.explorers;
+
+import glacialExpedition.common.ExceptionMessages;
+import glacialExpedition.models.suitcases.Carton;
+import glacialExpedition.models.suitcases.Suitcase;
+
+public abstract class BaseExplorer implements Explorer{
+    private String name;
+    private double energy;
+    private Suitcase suitcase;
+
+
+    protected BaseExplorer(String name, double energy) {
+        setName(name);
+        setEnergy(energy);
+        this.suitcase = new Carton();
+    }
+
+    @Override
+    public void search() {
+        if (this.energy <= 15) {
+            this.energy = 0;
+        } else {
+            this.energy -= 15;
+        }
+    }
+    @Override
+    public boolean canSearch () {
+        return energy > 0;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.EXPLORER_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+
+        if (energy < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.EXPLORER_ENERGY_LESS_THAN_ZERO);
+        }
+
+        this.energy = energy;
+    }
+
+    @Override
+    public Suitcase getSuitcase() {
+        return suitcase;
+    }
+
+    public void setSuitcase(Suitcase suitcase) {
+        this.suitcase = suitcase;
+    }
+}
