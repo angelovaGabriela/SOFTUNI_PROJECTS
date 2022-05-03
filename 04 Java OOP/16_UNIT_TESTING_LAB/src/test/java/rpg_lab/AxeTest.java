86fc171e806137a309ddf39d75f33b593940b0cd
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-05-03 22:55:56 +0200

    Last added: new lines in a project from Unit Testing courses

--- 04 Java OOP/16_UNIT_TESTING_LAB/src/test/java/rpg_lab/AxeTest.java
+++ 04 Java OOP/16_UNIT_TESTING_LAB/src/test/java/rpg_lab/AxeTest.java
@@ -1,24 +1,35 @@
 package rpg_lab;
 
-import junit.framework.TestCase;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class AxeTest extends TestCase {
+public class AxeTest  {
+    private static final int AXE_ATTACK = 10;
+    private static final int AXE_DURABILITY = 10;
 
-    // Test if weapon loses durability after each attack;
+    private static final int HEALTH = 100;
+    private static final int EXPERIENCE = 100;
+    private Dummy dummy;
+    private Axe axe;
+    private Axe brokenAxe;
 
+   @Before
+   public void setUp() {
+       this.axe = new Axe(AXE_ATTACK,AXE_DURABILITY);
+       this.brokenAxe = new Axe(AXE_ATTACK,0);
+       this.dummy = new Dummy(HEALTH, EXPERIENCE);
+   }
     @Test
-    public void testWeaponLosesDurabilityAfterAttack() {
-
-         // Arrange
-         Axe axe = new Axe(10, 10);
-         Dummy dummy = new Dummy(100, 100);
-         // Act
+    public void testAxeLosesDurabilityAfterAttack() {
          axe.attack(dummy);
+         Assert.assertEquals(AXE_ATTACK - 1, axe.getDurabilityPoints());
+    }
 
-         // Assert - expectations vs reality
-        Assert.assertEquals(9, axe.getDurabilityPoints());
+    @Test(expected = IllegalStateException.class)
+    public void testAxeAttackThrowsIfAxeIsBroken() {
+        brokenAxe.attack(dummy);
     }
 
 }
