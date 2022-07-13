bde03666e76996091dde793d128e6caa17a0864c
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-07-13 16:19:46 +0300

    Last added: brands and offers drop down created in addOffer/mobilele.

---
+++ 06 Spring Fundamentals/mobilele/src/main/java/bg/softuni/mobilele/repository/BrandRepository.java
@@ -0,0 +1,11 @@
+package bg.softuni.mobilele.repository;
+
+import bg.softuni.mobilele.model.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+
+
+}
