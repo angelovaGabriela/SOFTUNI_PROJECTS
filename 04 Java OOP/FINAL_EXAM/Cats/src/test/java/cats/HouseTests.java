108069fd742ee940f0f6131fd953ca5c8be5083b
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-07-22 16:21:11 +0300

    Last added: new unit tests for Cat project created.

--- 04 Java OOP/FINAL_EXAM/Cats/src/test/java/cats/HouseTests.java
+++ 04 Java OOP/FINAL_EXAM/Cats/src/test/java/cats/HouseTests.java
@@ -20,4 +20,46 @@ public class HouseTests {
         House expected = new House("VanesaHouse", 219);
     }
 
+    @Test
+    public void testGetHouseNameMustReturnCorrectName() {
+        String expected = "VanesaHouse";
+        String actual = house.getName();
+
+        Assert.assertEquals("The name isn't correct", expected, actual);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetNameMustThrowExceptionWhenNull() {
+       House newHouse = new House(null, 123);
+    }
+
+    @Test
+    public void getCapacityShouldReturn219() {
+        int expected = 219;
+        int actual = house.getCapacity();
+
+        Assert.assertEquals("Capacity isn't correct", expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getCapacityTestBelowOrEqualTo0MustThrowException() {
+        House pugHouse = new House("Pug house", -2);
+    }
+
+    @Test
+    public void getCountTestShouldReturn1() {
+        int expected = 1;
+        house.addCat(cat);
+        int actual = house.getCount();
+
+        Assert.assertEquals("Count of cats isn't correct.", expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenHouseIsFullAddCatMustThrowException() {
+        House naughtyCats = new House("Naughty cats", 1);
+        naughtyCats.addCat(cat);
+        Cat extraCat = new Cat("Pongo");
+        naughtyCats.addCat(extraCat);
+    }
 }
