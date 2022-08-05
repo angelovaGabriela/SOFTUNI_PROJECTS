9507814fbd7e9371c8a07361f2ff57d2390e1b38
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-08-05 18:24:10 +0300

    Last added: methods logic implemented for BaseProduct abstract class // OnlineShop project from OOP exam preparation

--- 04 Java OOP/FINAL_EXAM/OnlineShop/src/onlineShop/models/products/BaseProduct.java
+++ 04 Java OOP/FINAL_EXAM/OnlineShop/src/onlineShop/models/products/BaseProduct.java
@@ -1,5 +1,7 @@
 package onlineShop.models.products;
 
+import onlineShop.common.constants.ExceptionMessages;
+
 public abstract class BaseProduct implements Product {
 
     private int id;
@@ -9,66 +11,78 @@ public abstract class BaseProduct implements Product {
     private double overallPerformance;
 
     protected BaseProduct(int id, String manufacturer, String model, double price, double overallPerformance) {
-        this.id = id;
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.price = price;
+        this.setId(id);
+        this.setManufacturer(manufacturer);
+        this.setModel(model);
+        this.setPrice(price);
         this.overallPerformance = overallPerformance;
     }
 
     @Override
     public int getId() {
-        return id;
+        return this.id;
     }
 
     private void setId(int id) {
-        this.id = id;
+       if (this.id <= 0) {
+           throw new IllegalArgumentException(ExceptionMessages.INVALID_PRODUCT_ID);
+       }
+       this.id = id;
     }
 
     @Override
     public String getManufacturer() {
-        return manufacturer;
+        return this.manufacturer;
     }
 
     private void setManufacturer(String manufacturer) {
+        if (manufacturer == null || manufacturer.trim().isEmpty()) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_MANUFACTURER);
+        }
         this.manufacturer = manufacturer;
     }
 
     @Override
     public String getModel() {
-        return model;
+        return this.model;
     }
 
     private void setModel(String model) {
-        this.model = model;
+       if (model == null || model.trim().isEmpty()) {
+           throw new IllegalArgumentException(ExceptionMessages.INVALID_MODEL);
+       }
+       this.model = model;
     }
 
     @Override
     public double getPrice() {
-        return price;
+        return this.price;
     }
 
     private void setPrice(double price) {
-        this.price = price;
+       if (price <= 0) {
+           throw new IllegalArgumentException(ExceptionMessages.INVALID_PRICE);
+       }
+       this.price = price;
     }
 
     @Override
     public double getOverallPerformance() {
-        return overallPerformance;
+        return this.overallPerformance;
     }
 
     private void setOverallPerformance(double overallPerformance) {
+        if (overallPerformance <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_OVERALL_PERFORMANCE);
+        }
         this.overallPerformance = overallPerformance;
     }
 
     @Override
     public String toString() {
-        return "BaseProduct{" +
-                "id=" + id +
-                ", manufacturer='" + manufacturer + '\'' +
-                ", model='" + model + '\'' +
-                ", price=" + price +
-                ", overallPerformance=" + overallPerformance +
-                '}';
+        return String.format("Overall Performance: %f. Price: %f - %s: %s %s (Id: %d)",
+                this.overallPerformance, this.price,
+                this.getClass().getSimpleName(),
+                this.manufacturer, this.model, this.id);
     }
 }
