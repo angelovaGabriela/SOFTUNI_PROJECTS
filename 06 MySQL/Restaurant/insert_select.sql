93449c0856b7dee4d14cd39959d1bd2e1ffa8365
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-11-11 14:53:06 +0100

    Last added: INSERT/SELECT query Section 2: Data Manipulation Language (DML) from Restaurant problem // Exam preparation for MySQL exam

---
+++ 06 MySQL/Restaurant/insert_select.sql
@@ -0,0 +1,14 @@
+use restaurant_db;
+
+INSERT INTO products (name,type,price)
+
+SELECT concat(last_name,' ',"specialty"), 'Cocktail', ceiling(salary*0.01)
+FROM waiters
+WHERE waiters.id > 6 
+
+##INSERT INTO table2 (column1, column2, column3, ...)
+##SELECT column1, column2, column3, ...
+##FROM table1
+##WHERE condition;
+ 
+ 
