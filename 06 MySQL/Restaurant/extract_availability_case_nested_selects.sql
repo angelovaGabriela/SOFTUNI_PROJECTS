6830655eaf5b6816545644a841e40913a84a654f
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-11-21 15:27:12 +0100

    Last added: Full group by

--- 06 MySQL/Restaurant/extract_availability_case_nested_selects.sql
+++ 06 MySQL/Restaurant/extract_availability_case_nested_selects.sql
@@ -1,6 +1,6 @@
+use restaurant_db;
 
-
-SELECT (SELECT table_id FROM orders WHERE table_id = tables.id LIMIT 1) as table_id, capacity, (SELECT COUNT(distinct client_id) FROM orders_clients WHERE order_id IN (SELECT id FROM orders WHERE table_id = tables.id)) as count_clients, 
+SELECT (SELECT table_id FROM orders WHERE table_id = tables.id LIMIT 1) as table_id, capacity, (SELECT COUNT(client_id) FROM orders_clients WHERE order_id IN (SELECT id FROM orders WHERE table_id = tables.id)) as count_clients, 
 CASE
 WHEN capacity > (SELECT COUNT(client_id) FROM orders_clients WHERE order_id IN (SELECT id FROM orders WHERE table_id = tables.id)) THEN "Free seats"
 WHEN capacity < (SELECT COUNT(client_id) FROM orders_clients WHERE order_id IN (SELECT id FROM orders WHERE table_id = tables.id)) THEN "Extra seats"
@@ -9,6 +9,8 @@ END as availability
 FROM tables
 
 WHERE tables.floor = 1 
-GROUP BY table_id, count_clients
+GROUP BY table_id, capacity, count_clients, availability
 ORDER BY table_id  DESC;
+
+
 
