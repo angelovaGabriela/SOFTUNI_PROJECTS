ebd6d522dfa4ca1caa62b07becc6bda147a6ff08
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-11-26 13:42:55 +0100

    Last added: left join query
    Section 3: Querying – task 7
    
    // MySQL exam preparation

---
+++ 06 MySQL/Movie database/extract_left_join.sql
@@ -0,0 +1,14 @@
+USE softuni_imdb;
+
+SELECT CONCAT(`a`.`first_name`, ' ', `a`.`last_name`) AS `full_name`,
+CONCAT(REVERSE(`a`.`last_name`), CHAR_LENGTH(`a`.`last_name`), '@cast.com') AS `email`,
+(2022 - YEAR(`a`.`birthdate`)) AS `age`,
+`a`.`height`
+FROM `actors`AS `a`
+LEFT JOIN `movies_actors` AS `ma`
+ON `a`.`id` = `ma`.`actor_id`
+WHERE `ma`.`actor_id` IS NULL
+ORDER BY `a`.`height` ASC;
+
+
+
