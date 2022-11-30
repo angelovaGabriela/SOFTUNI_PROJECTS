e8df713250ba2c9267c9f47e19d51b0d90ee2d76
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2022-11-30 17:49:00 +0100

    Last added: insert, update, delete
    Section 2: Data Manipulation Language (DML) – task 2/3/4
    
    // MySQL exam preparation

---
+++ 06 MySQL/Online store/delete_left_join.sql
@@ -0,0 +1,6 @@
+USE online_store;
+
+DELETE c FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL;
---
+++ 06 MySQL/Online store/insert_select.sql
@@ -0,0 +1,11 @@
+USE online_store;
+
+
+INSERT INTO reviews (`content`, `picture_url`, `published_at`, `rating`)
+
+SELECT SUBSTRING(p.description, 1, 15), 
+REVERSE(p.name),
+('2010-10-10'),
+(p.price / 8) 
+FROM products AS p 
+WHERE p.id >= 5;
