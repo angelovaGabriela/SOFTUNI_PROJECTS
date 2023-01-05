e05fa6c50d676c223fe39d473c11e6bbf18ff508
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-01-05 16:10:19 +0100

    Last added: sum_of_numbers solved correctly

--- 08 JS Advanced/Syntax Functions and Statements/sum_of_numbers.js
+++ 08 JS Advanced/Syntax Functions and Statements/sum_of_numbers.js
@@ -5,13 +5,12 @@ function solve(m, n) {
     let num1 = Number(m);
     let num2 = Number(n)
 
-    let result;
-    for(let i = num1; i < num2; i++) {
-        let number = [i];
-        result += number;
+    let result = 0;
+    for(let i = num1; i <= num2; i++) {
+        result += i;
     }
 
-    return console.log(result);
+    console.log(result);
 }
 
 solve('1', '5');
