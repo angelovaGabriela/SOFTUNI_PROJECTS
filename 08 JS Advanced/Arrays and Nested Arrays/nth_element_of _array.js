2902a2b891271b5a57891b227bd4205fa93f5389
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-01-13 14:46:20 +0100

    Last added: print every N-th element JS arrays exercices - problem solution 2

---
+++ 08 JS Advanced/Arrays and Nested Arrays/nth_element_of _array.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function printEveryNthElement(array, step) {
+
+    let result = [];
+    for(let i = 0; i < array.length; i += step) {
+
+        result.push(array[i]);
+            
+    }
+    return result;
+}
+
+printEveryNthElement(['5','20','31','4','20'],2)
