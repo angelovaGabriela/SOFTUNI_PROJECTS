96c885e2665b4cb12728ca6d10286322f8329b8a
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-01-25 19:52:50 +0100

    Last added: update

--- 08 JS Advanced/04_DOM_INTRODUCTION/03.Sum Numbers/SumNumbers.js
+++ 08 JS Advanced/04_DOM_INTRODUCTION/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,11 @@
+'use strict'
+
 function calc() {
-    // TODO: sum = num1 + num2
+
+    let firstNumber = Number(document.getElementById('num1').value);
+    let secondNumber = Number(document.getElementById('num2').value);
+
+    let sum = document.getElementById('sum');
+
+    sum.value = firstNumber + secondNumber;
 }
