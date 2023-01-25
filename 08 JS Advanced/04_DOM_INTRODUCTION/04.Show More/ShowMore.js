7ac2c191122f0036ee5032ad0b82bef9fe57a5f1
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-01-25 20:23:10 +0100

    Last added: update

--- 08 JS Advanced/04_DOM_INTRODUCTION/04.Show More/ShowMore.js
+++ 08 JS Advanced/04_DOM_INTRODUCTION/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+  let getLink = document.querySelector('#more');
+  let hiddenText = document.querySelector('#text');
+
+  getLink.style.display = 'none';
+  hiddenText.style.display = 'inline';
 }
