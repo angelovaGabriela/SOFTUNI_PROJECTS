dc1e1baf259295a8c1388e54f49bd299263e33e1
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-01-26 16:08:40 +0100

    Last added: update

--- 08 JS Advanced/04_DOM_INTRODUCTION/05.Colorize Table/ColorizeTable.js
+++ 08 JS Advanced/04_DOM_INTRODUCTION/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,11 @@
+'use strict'
+
 function colorize() {
-    // TODO
+    let tableRows = Array.from(document.querySelectorAll('table tr'));
+
+    for(let i = 1; i < tableRows.length; i++) {
+        if(i % 2 !== 0) {
+            tableRows[i].style.backgroundColor = "Teal";
+        }
+    }
 }
