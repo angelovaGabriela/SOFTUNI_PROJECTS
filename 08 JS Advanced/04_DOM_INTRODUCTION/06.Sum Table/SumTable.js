d311fc25121b7c1563bc17ef1e1d500e90a422d7
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-01-26 16:23:56 +0100

    Last added: update

--- 08 JS Advanced/04_DOM_INTRODUCTION/06.Sum Table/SumTable.js
+++ 08 JS Advanced/04_DOM_INTRODUCTION/06.Sum Table/SumTable.js
@@ -1,3 +1,18 @@
+'use strict'
+
 function sumTable() {
 
+    let tableRows = document.querySelectorAll('table tbody tr td');
+    let totalField = document.querySelector('#sum');
+    let total = 0;
+
+    for(let i = 0; i < tableRows.length - 1; i++) {
+
+        if(parseInt(tableRows[i].textContent)) {
+
+            total += Number(tableRows[i].textContent);
+        }
+    }
+
+    totalField.textContent = total;
 }
