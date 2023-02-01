f26024d5c82c19af1f6c12095bc558db8782d10e
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-01 18:53:19 +0100

    Last added: update

--- 08 JS Advanced/04_DOM_INTRODUCTION/04. Search in List_exercice/search.js
+++ 08 JS Advanced/04_DOM_INTRODUCTION/04. Search in List_exercice/search.js
@@ -1,3 +1,26 @@
 function search() {
-   // TODO
+   let getTowns = Array.from(document.querySelectorAll('ul li'));
+   let getInput = document.getElementById('searchText').value;
+   let matches = 0;
+
+
+   for (let city of getTowns) {
+      city.style.textDecoration = null;
+      city.style, fontWeigth = null;
+   }
+
+   for (let town of getTowns) {
+
+      if (town.textContent.includes(getInput)) {
+         town.style.textDecoration = 'underline';
+         town.style.fontWeight = 'bold';
+         matches++;
+
+
+      }
+
+
+   }
+
+   document.getElementById('result').textContent = `${matches} matches found`;
 }
