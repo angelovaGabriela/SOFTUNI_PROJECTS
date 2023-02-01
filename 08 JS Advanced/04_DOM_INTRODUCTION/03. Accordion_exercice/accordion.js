ef4ff0f9711f1082eea15400d3878659c84aa9bd
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-01 18:51:38 +0100

    Last added: update

--- 08 JS Advanced/04_DOM_INTRODUCTION/03. Accordion_exercice/accordion.js
+++ 08 JS Advanced/04_DOM_INTRODUCTION/03. Accordion_exercice/accordion.js
@@ -1,14 +1,14 @@
 'use strict'
 
 function toggle() {
-  let getButton = document.querySelector('.button');
-  let getText = document.querySelector('#extra');
+    let getButton = document.querySelector('.button');
+    let getText = document.querySelector('#extra');
 
-if (getButton.textContent === 'More') {
-    getButton.textContent = 'Less';
-    getText.style.display = 'block';
-} else {
-    getButton.textContent = 'More';
-    getText.style.display = 'none'
-}
+    if (getButton.textContent === 'More') {
+        getButton.textContent = 'Less';
+        getText.style.display = 'block';
+    } else {
+        getButton.textContent = 'More';
+        getText.style.display = 'none'
+    }
 }
