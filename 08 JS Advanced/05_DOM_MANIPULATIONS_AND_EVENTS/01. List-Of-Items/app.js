5a03544359a39d53c8dc2cff5b12422c06e4bf1d
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-02 21:56:33 +0100

    Last added: problem solution 1

--- 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/01. List-Of-Items/app.js
+++ 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
+'use strict'
+
 function addItem() {
-    console.log('TODO:...');
+    let input = document.getElementById('newItemText').value;
+    let collection = document.querySelector('ul');
+
+    let li = document.createElement('li');
+    li.textContent = input;
+    collection.appendChild(li);
+
+    input.value = '';
 }
