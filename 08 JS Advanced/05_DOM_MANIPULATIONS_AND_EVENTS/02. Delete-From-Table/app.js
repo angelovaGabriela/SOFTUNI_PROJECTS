983531483f5e75a834da35ea2f4383184bba7f01
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-02 23:36:36 +0100

    Last added: problem solution 2

--- 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/02. Delete-From-Table/app.js
+++ 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
+'use strict'
 function deleteByEmail() {
-    console.log('TODO:...');
+   let input = document.querySelector('input').value;
+   let resultElement = document.getElementById('result');
+   let customerList = document.querySelectorAll('td:nth-child(2)');
+
+   for (let item of customerList) {
+    
+    if(item.textContent === input) {
+        item.parentNode.remove();
+        resultElement.textContent = "Deleted."
+        return;
+    }
+    resultElement.textContent = "Not found.";
+
+   }
 }
