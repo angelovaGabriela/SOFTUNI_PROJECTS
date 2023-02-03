80349bd58c2b133c447af8bca590bda98528affc
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-03 12:17:07 +0100

    Last added: problem solution 4

--- 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/04. Mouse-Gradient/app.js
+++ 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
-}
+    let result = document.getElementById('result');
+    let gradient = document.getElementById('gradient');
+
+    gradient.addEventListener('mousemove', getPercentage);
+      function getPercentage(event) {
+        result.textContent = `${Math.floor((event.offsetX / gradient.clientWidth) * 100)}%`; 
+      }
+    }
