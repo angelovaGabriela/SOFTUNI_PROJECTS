415ecdca263149ed9bf73211d8b3e1775129c178
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-03 11:44:32 +0100

    Last added: problem solution 3

--- 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/03. Add-Delete/app.js
+++ 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newItemText').value;
+    let listElements = document.getElementById('items');
+
+    let newElement = document.createElement('li');
+    newElement.textContent = inputElement;
+    listElements.appendChild(newElement);
+
+    let deleteElement = document.createElement('a');
+    deleteElement.setAttribute('href', '#');
+    deleteElement.textContent = "[Delete]";
+   
+
+    newElement.appendChild(deleteElement);
+
+    deleteElement.addEventListener('click', deleteClick);
+
+
+    function deleteClick(event) {
+        event.currentTarget.parentNode.remove();
+    }
+
+    inputElement.value = '';
 }
