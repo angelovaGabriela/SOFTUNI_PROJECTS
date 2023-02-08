b65660b32c3d89d8d80d11932f9d4d6e0b8c0122
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-08 15:42:28 +0100

    Last added: problem solution 6 completed

--- 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/06. Dynamic-Validation/app.js
+++ 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.getElementById("email");
+
+    input.addEventListener('change', changed);
+
+    function changed(event) {
+        let validEmail = /[a-z]+@[a-z]+\.[a-z]+/g;
+
+        if (validEmail.test(input.value)) {
+            event.target.classList = '';
+        } else {
+            event.target.setAttribute("class", "error");
+        }
+
+    }
+
 }
