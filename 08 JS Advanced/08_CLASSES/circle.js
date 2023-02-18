948e4f52f1f30fcabe6af5bd38ad1b3cc057f0e4
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-18 14:47:14 +0100

    Last added: problem solution 3

---
+++ 08 JS Advanced/08_CLASSES/circle.js
@@ -0,0 +1,38 @@
+//Area of a circle = π × r 2.
+//let x = Math.PI;
+
+class Circle {
+    radius
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    set diameter(value) {
+        return this.radius = value / 2;
+    }
+
+    get area() {
+        return Math.PI * this.radius ** 2;
+    }
+}
+
+
+let c = new Circle(2);
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
+
+c.diameter = 1.6;
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
