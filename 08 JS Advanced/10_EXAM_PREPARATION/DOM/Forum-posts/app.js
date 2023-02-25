a9f11aa6c7b0226dcc0b4e0d0e55c3c0e7550d53
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-02-25 15:58:45 +0100

    Last added: DOM task "Forum posts" completed

--- 08 JS Advanced/10_EXAM_PREPARATION/DOM/Forum-posts/app.js
+++ 08 JS Advanced/10_EXAM_PREPARATION/DOM/Forum-posts/app.js
@@ -1,33 +1,38 @@
 window.addEventListener("load", solve);
-function solve () {
+function solve() {
     let publish = document.getElementById("publish-btn");
     publish.addEventListener("click", createPost);
+
+    let clear = document.getElementById("clear-btn");
+    clear.addEventListener("click", clearPosts);
 
     let title = document.getElementById("post-title");
     let category = document.getElementById("post-category");
     let content = document.getElementById("post-content");
-    let ul = document.getElementById("review-list");
-    
-    function createPost (event) {
+    let reviewList = document.getElementById("review-list");
+    let publishedList = document.getElementById("published-list");
+
+
+    function createPost(event) {
         let titleValue = title.value;
         let categoryValue = category.value;
         let contentValue = content.value;
 
-    if(!titleValue || !categoryValue || !content.value) {
-        return;
-    }    
+        if (!titleValue || !categoryValue || !content.value) {
+            return;
+        }
 
-    createElements(titleValue, categoryValue, contentValue);
-    clearInputFields ();
-    
+        createElements(titleValue, categoryValue, contentValue);
+        clearInputFields();
+
     }
 
 
 
-    function createElements (titleValue, categoryValue, contentValue) {
+    function createElements(titleValue, categoryValue, contentValue) {
         let li = document.createElement("li");
         li.classList.add("rpost");
-    
+
         let article = document.createElement("article");
         let titleElement = document.createElement("h4");
         let categoryElement = document.createElement("p");
@@ -39,12 +44,13 @@ function solve () {
 
         let approveButton = document.createElement("button");
         approveButton.classList.add("action-btn", "approve");
+        approveButton.addEventListener("click", approvePost);
 
         appendElements(li, article, titleElement, categoryElement, contentElement, editButton, approveButton);
-        addTextContent (titleElement, categoryElement, contentElement, editButton, approveButton, titleValue, categoryValue, contentValue);
+        addTextContent(titleElement, categoryElement, contentElement, editButton, approveButton, titleValue, categoryValue, contentValue);
     }
 
-    function addTextContent (titleElement, categoryElement, contentElement, editButton, approveButton, titleValue, categoryValue, contentValue) {
+    function addTextContent(titleElement, categoryElement, contentElement, editButton, approveButton, titleValue, categoryValue, contentValue) {
 
         titleElement.textContent = titleValue;
         categoryElement.textContent = `Category: ` + categoryValue;
@@ -52,47 +58,61 @@ function solve () {
 
         editButton.textContent = "Edit";
         approveButton.textContent = "Approve";
-    
+
     }
 
-    function appendElements (li, article, titleElement, categoryElement, contentElement, editButton, approveButton) {
-       
-        ul.appendChild(li);
+    function appendElements(li, article, titleElement, categoryElement, contentElement, editButton, approveButton) {
+
+        reviewList.appendChild(li);
         li.appendChild(article);
         article.appendChild(titleElement);
         article.appendChild(categoryElement);
         article.appendChild(contentElement);
-    
+
         li.appendChild(editButton);
         li.appendChild(approveButton);
 
     }
 
-    function clearInputFields () {
+    function clearInputFields() {
         title.value = '';
         category.value = '';
-        content.value = '';    
+        content.value = '';
     }
-    
-    function editPost (event) {
-        // choosing the post where the edit button is and getting his home (parentElement)
-       let currentPost = event.target.parentElement;
-       // getting the first article
-       let articleContent = currentPost.getElementsByTagName("article")[0].children;
 
-       // elements from the article and their text (value);
-       let titleValue  = articleContent[0].textContent;
-       let categoryValue  = articleContent[1].textContent;
-       let contentValue = articleContent[2].textContent;
+    function editPost(event) {
 
-       // return back the text to the input fields for editin
+        let currentPost = event.target.parentElement;
+        let articleContent = currentPost.getElementsByTagName("article")[0].children;
 
-       title.value = titleValue;
-       category.value = categoryValue.split(":")[1];
-       content.value = contentValue.split(":")[1];
+        let titleValue = articleContent[0].textContent;
+        let categoryValue = articleContent[1].textContent;
+        let contentValue = articleContent[2].textContent;
 
-       // removing the current post
-       currentPost.remove();
-       
-    } 
+
+        title.value = titleValue;
+        category.value = categoryValue.split(": ")[1];
+        content.value = contentValue.split(": ")[1];
+
+
+        currentPost.remove();
+
+    }
+
+    function approvePost(event) {
+
+        let node = event.target.parentElement;
+        let postCopy = node.cloneNode(true);
+
+        let removeEditApprove = postCopy.getElementsByTagName("button");
+        Array.from(removeEditApprove).forEach(b => b.remove());
+
+        event.target.parentElement.remove();
+        publishedList.appendChild(postCopy);
+    }
+
+    function clearPosts(event) {
+        Array.from(publishedList.children).forEach(p => p.remove());
+
+    }
 }
