b8dd391359f228b84cabcf9fe9369cdf7f992c0d
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-03-01 17:51:46 +0100

    Last added: Shoping Cart task succesfully completed

--- 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/07. Shopping-Cart/solution.js
+++ 08 JS Advanced/05_DOM_MANIPULATIONS_AND_EVENTS/07. Shopping-Cart/solution.js
@@ -10,27 +10,21 @@ function solve() {
       let currentProduct = event.target.parentElement.parentElement; 
       let collection = Array.from(currentProduct.children);
       let productName = collection[1].firstElementChild.innerHTML;
-      let price = Number(collection[3].innerHTML).toFixed(2);
+      let price = collection[3].innerHTML;
+
       shoppingList.add(productName);
       textarea.value += `Added ${productName} for ${price} to the cart.\n`
+
+      totalMoney += parseFloat(price);
+      
    }
 
    let checkoutButton = document.getElementsByClassName("checkout")[0];
    checkoutButton.addEventListener("click", checkout) 
 
-  function checkout (event) {
+  function checkout () {
 
-   let arrayOfProducts = Array.from(shoppingList);
-
-   for(let product of arrayOfProducts) {
-      if(product === "Bread"){
-         totalMoney += 0.80;
-      } else if (product === "Tomatoes") {
-         totalMoney += 0.99;
-      } else if (product === "Milk") {
-         totalMoney += 1.09;
-      }
-   }
+  let arrayOfProducts = Array.from(shoppingList);
 
   textarea.value += `You bought ${arrayOfProducts.join(", ")} for ${totalMoney.toFixed(2)}.`
 
