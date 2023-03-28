2b5cbec032c30c85890024cec7780e4294c837b9
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-03-28 16:35:42 +0200

    Last added: User registration functionality building started

---
+++ 07 Spring Fundamentals/shoppingList/src/main/java/com/example/shoppingList/web/UserController.java
@@ -0,0 +1,45 @@
+package com.example.shoppingList.web;
+
+import com.example.shoppingList.model.binding.UserRegisterBindingModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult,
+                                  RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors() || !userRegisterBindingModel.getPassword()
+                .equals(userRegisterBindingModel.getConfirmPassword())) {
+            redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
+
+            return "redirect:register";
+        }
+            return "redirect:login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel () {
+        return new UserRegisterBindingModel();
+    }
+}
