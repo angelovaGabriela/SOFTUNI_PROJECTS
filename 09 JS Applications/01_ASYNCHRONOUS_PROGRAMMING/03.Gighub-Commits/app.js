25f8fbda2300e51c3dd473730dca4e9bc7be57fe
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-06-28 12:18:17 +0200

    Last added: Github Commits task completed - async function

--- 09 JS Applications/01_ASYNCHRONOUS_PROGRAMMING/03.Gighub-Commits/app.js
+++ 09 JS Applications/01_ASYNCHRONOUS_PROGRAMMING/03.Gighub-Commits/app.js
@@ -1,4 +1,34 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+	const username = document.getElementById('username').value;
+	const repository = document.getElementById('repo').value;
+
+
+    const response = await fetch(`https://api.github.com/repos/${username}/${repository}/commits`)
+
+    try {
+        if (response.status == 404) {
+			throw new Error(`Error: ${response.status} (Not Found)`);
+		}
+		const data = await response.json();
+
+        const list = getElementById('commits');
+
+        const items = data.map(info => {
+			const li = document.createElement('li');
+			li.textContent = `${info.commit.author.name}: ${info.commit.message}`
+			list.appendChild(li);
+
+			return li;
+        });
+
+        list.replaceChildren(...items);
+
+
+
+    } catch(err) {
+        const list = document.getElementById('commits');
+		list.textContent = err.message;
+        
+    }
+    
 }
