414c7cb3f39884e18755e72f2d52950fbb494e3a
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-06-29 14:51:26 +0200

    Last added: Bus-Stop task completed

--- 09 JS Applications/01_ASYNCHRONOUS_PROGRAMMING/Bus-Stop/app.js
+++ 09 JS Applications/01_ASYNCHRONOUS_PROGRAMMING/Bus-Stop/app.js
@@ -1,3 +1,33 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+
+    
+    const busStopId = document.getElementById('stopId').value;
+    const busesList = document.getElementById('buses');
+
+    busStopId.innerHtml = "";
+    busesList.innerHTML = "";
+
+    try {
+
+        const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopId}`);
+
+        if (response.ok === false) {
+            throw new Error();
+        }
+
+        const data = await response.json();
+
+        const stopName = document.getElementById('stopName');
+        stopName.textContent = data.name;
+
+        Object.entries(data.buses).forEach(([busId, arrivalTime]) => {
+            const li = document.createElement('li');
+            li.textContent = `Bus ${busId} arrives in ${arrivalTime} minutes`;
+            busesList.appendChild(li)
+        });
+
+    } catch (err) {
+        stopName.textContent = "Error";
+    }
+
 }
