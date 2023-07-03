fa00651c59f260fcb7bbc886603ea2bc8efed8e8
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-03 13:38:42 +0200

    Last added: Blog task solved

--- 09 JS Applications/01_ASYNCHRONOUS_PROGRAMMING/Blog/app.js
+++ 09 JS Applications/01_ASYNCHRONOUS_PROGRAMMING/Blog/app.js
@@ -1,7 +1,9 @@
 function attachEvents() {
-     const postButton = document.getElementById('btnLoadPosts');
-     postButton.addEventListener('click', getPosts);
-  //  document.getElementById('btnViewPost').addEventListener('click', getComments);
+    const postButton = document.getElementById('btnLoadPosts');
+    postButton.addEventListener('click', getPosts);
+
+    const viewButton = document.getElementById('btnViewPost');
+    viewButton.addEventListener('click', getComments);
 }
 
 
@@ -9,33 +11,57 @@ async function getPosts() {
     const dropDownMenu = document.getElementById('posts');
     dropDownMenu.replaceChildren();
 
-    try {
-        const response = await fetch('http://localhost:3030/jsonstore/blog/posts');
 
-        if (!response.ok || response.status != 200) {
-            throw new Error('Invalid request');
-        }
-        const data = await response.json();
+    const response = await fetch('http://localhost:3030/jsonstore/blog/posts');
 
-        Object.values(data).forEach(post => {
-            const newOption = document.createElement('option');
-            newOption.value = post.id;
-            newOption.textContent = post.title;
-            dropDownMenu.appendChild(newOption);
-        });
+    const data = await response.json();
 
-    } catch (error) {
-        alert(error.message);
-    }
+    Object.values(data).forEach(post => {
+        const newOption = document.createElement('option');
+        newOption.value = post.id;
+        newOption.textContent = post.title;
+        dropDownMenu.appendChild(newOption);
+    });
 
 
-  
+
+
+
 }
 
-// async function getComments() {
-//     const response = await fetch('http://localhost:3030/jsonstore/blog/comments');
-//     const data = await response.json();
+async function getComments() {
+
+    const selectedPost = document.getElementById('posts').selectedOptions[0].value;
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postComments = document.getElementById('post-comments');
+    postComments.replaceChildren();
+
+    postTitle.textContent = "";
+
+    const response = await fetch(`http://localhost:3030/jsonstore/blog/posts/${selectedPost}`);
+    const postData = await response.json();
+
+    postTitle.textContent = postData.title.toUpperCase();
+    postBody.textContent = postData.body;
 
 
-// }
+
+
+    const urlComments = 'http://localhost:3030/jsonstore/blog/comments';
+    const commentsResponse = await fetch(urlComments);
+
+
+
+    const commentsData = await commentsResponse.json();
+
+    const commentsOfThePost = Object.values(commentsData).filter(comment => comment.postId === selectedPost);
+    commentsOfThePost.forEach(comment => {
+        let li = document.createElement('li');
+        li.id = comment.id;
+        li.textContent = comment.text;
+        postComments.appendChild(li);
+    })
+
+}
 attachEvents();
