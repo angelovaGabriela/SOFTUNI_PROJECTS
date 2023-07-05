883f96292ab14e4f61be2babb3bc01803beee2c3
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-05 13:38:06 +0200

    Last added: Messenger task completed

--- 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Messenger/app.js
+++ 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Messenger/app.js
@@ -1,5 +1,49 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("refresh").addEventListener("click", getAllMessages);
+    document.getElementById("submit").addEventListener("click", sendMessages);
+
+}
+
+async function getAllMessages() {
+    const url = `http://localhost:3030/jsonstore/messenger`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    renderMessage(data);
+}
+
+function renderMessage(data) {
+    const textArea = document.getElementById("messages");
+    const messages = Object.values(data).map(entry => `${entry.author}: ${entry.content}`).join('\n');
+    textArea.textContent = messages;
+}
+
+function sendMessages() {
+    const author = document.querySelector('input[name="author"]');
+    const content = document.querySelector('input[name="content"]');
+
+    const body = {
+        author: author.value,
+        content: content.value
+    }
+
+    author.value = "";
+    content.value = "";
+
+    createMessages(body)
+}
+
+async function createMessages(body) {
+    const response = await fetch('http://localhost:3030/jsonstore/messenger', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await response.json();
+    getAllMessages();
+  
 }
 
 attachEvents();
