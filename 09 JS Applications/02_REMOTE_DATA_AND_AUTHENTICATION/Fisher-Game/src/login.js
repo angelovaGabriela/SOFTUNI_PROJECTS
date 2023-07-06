16c428194a38e0ac19bb5c92388c80eb9a67b8a6
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-06 18:30:16 +0200

    Last added: register & login functionalities done

--- 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/login.js
+++ 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/login.js
@@ -1,1 +1,39 @@
-console.log('TODO:// Implement Login functionality');
+document.getElementById('login-form').addEventListener('submit', loginHeandler);
+document.querySelectorAll("a").forEach(x => x.classList.remove("active"));
+document.getElementById("login").classList.add("active");
+
+function loginHeandler(event) {
+    event.preventDefault();
+    const formElement = event.target;
+    const data = new FormData(formElement);
+    const {email, password} = Object.fromEntries(data);
+
+    onLogin(email, password);
+}
+
+async function onLogin(email, password) {
+
+    const url = `http://localhost:3030/users/login`;
+
+    const body = {
+        email,
+        password
+    }
+
+    const response = await fetch(url,  {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await response.json();
+
+    sessionStorage.setItem("email", data.email);
+    sessionStorage.setItem("accessToken", data.accessToken);
+    
+
+    window.location = "./index.html";
+    return data;
+   
+}
--- 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/register.js
+++ 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/register.js
@@ -1,51 +1,55 @@
 document.getElementById('register-form').addEventListener('submit', registerHandler);
-const showError =  document.getElementsByClassName('notification')[0];
+document.querySelectorAll("a").forEach(x => x.classList.remove("active"));
+document.getElementById("register").classList.add("active");
+
+const showError = document.getElementsByClassName('notification')[0];
 
 function registerHandler(event) {
     event.preventDefault();
     const formElement = event.target;
     const data = new FormData(formElement);
-    const {email, password, rePassword} = Object.fromEntries(data);
-   
-    if (password !== rePassword) {
-       showError.textContent = "ERROR"
-        setTimeout(()=> {
+    const { email, password, rePass } = Object.fromEntries(data);
+
+    if (password !== rePass) {
+        showError.textContent = "ERROR"
+        setTimeout(() => {
             showError.textContent = ""
         }, 1000);
+        
     }
-   
-   
-    onRegister(email, password);
 
+    
+    onRegister(email, password);
 }
 
 async function onRegister(email, password) {
-
-    const url = 'http://localhost:3030/users/register';
-    
-   const body = {
+    const body = {
         email,
         password
     }
     try {
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
-    })
+        const url = 'http://localhost:3030/users/register';
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
 
-    const data = await response.json();
-    
-    if(data.code !== 200) {
-        throw new Error(data.message)
+        const data = await response.json();
+
+        if (data.code !== 200) {
+            throw new Error(data.message)
+        }
+
+        sessionStorage.setItem("email", data.email);
+        sessionStorage.setItem("accessToken", data.accessToken);
+        return data;
+    } catch (e) {
+        showError.textContent = e;
+        setTimeout(() => {
+            showError.textContent = "";
+        }, 1000);
     }
-    return data;
-} catch(e) {
-    showError.textContent = e;
-    setTimeout(() => {
-        showError.textContent = "";
-    }, 1000);
-}
 }
