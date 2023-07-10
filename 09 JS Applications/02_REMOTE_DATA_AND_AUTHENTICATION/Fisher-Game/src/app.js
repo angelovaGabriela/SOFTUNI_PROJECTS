e13c82f2c099e72036532fab6b5534b94982711d
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-10 12:04:07 +0200

    Last added: update - load button functionality

--- 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/app.js
+++ 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/app.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", onLoadHTML);
 document.querySelectorAll("a").forEach(x => x.classList.remove("active"));
 document.getElementById("register").classList.add("active");
 document.getElementById("logout").addEventListener("click", onLogout)
-
+document.getElementsByClassName('load')[0].addEventListener('click', loadAllCatches);
 
 function onLoadHTML() {
     const token = sessionStorage.getItem("authToken");
@@ -28,4 +28,48 @@ async function onLogout(e) {
 
     sessionStorage.clear(); 
     window.location.href = './index.html';
+}
+
+async function loadAllCatches() {
+    const url = 'http://localhost:3030/data/catches';
+    const response = await fetch(url);
+    const data = await response.json();
+ 
+  renderCatches(data);
+}
+
+function renderCatches(data) {
+
+    const catchDIV = document.getElementsByClassName('catch')[0];
+    const main = document.getElementById('main');
+    main.replaceChildren();
+    Object.values(data).forEach(c => {
+       const newCatchDIV = catchDIV.cloneNode(true);
+        main.appendChild(newCatchDIV);
+        const newAngler = newCatchDIV.getElementsByClassName('angler')[0];
+        newAngler.value = `${c.angler}`;
+        
+        const newWeight= newCatchDIV.getElementsByClassName('weight')[0];
+        newWeight.value = `${c.weight}`;
+
+        const newSpecies= newCatchDIV.getElementsByClassName('species')[0];
+        newSpecies.value = `${c.species}`;
+
+        const newLocation= newCatchDIV.getElementsByClassName('location')[0];
+        newLocation.value = `${c.location}`;
+        
+        const newBait = newCatchDIV.getElementsByClassName('bait')[0];
+        newBait.value = `${c.bait}`;
+        
+
+        const newCaptureTime = newCatchDIV.getElementsByClassName('captureTime')[0];
+        newCaptureTime.value = `${c.captureTime}`;
+
+    })
+        
+      
+    
+
+
+    
 }
