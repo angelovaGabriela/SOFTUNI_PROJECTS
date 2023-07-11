c378affe030e339770dd2b3ef5509dbe1d5aa90b
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-11 14:42:24 +0200

    Last added: update - "add catches" error handling

--- 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/app.js
+++ 09 JS Applications/02_REMOTE_DATA_AND_AUTHENTICATION/Fisher-Game/src/app.js
@@ -8,21 +8,21 @@ addButton.addEventListener('click', addCatch);
 
 function onLoadHTML() {
     const token = sessionStorage.getItem("authToken");
-     if(token) {
+    if (token) {
         document.getElementById("guest").style.display = "none";
         document.getElementById("user").style.display = "inline-block";
         document.getElementsByTagName('span')[0].textContent = sessionStorage.getItem('userEmail');
         addButton.disabled = false;
-     } else {
+    } else {
         document.getElementById("guest").style.display = "inline-block";
         document.getElementById("user").style.display = "none";
         document.getElementsByTagName('span')[0].textContent = 'guest';
         addButton.disabled = true;
-     }
+    }
 }
 
 async function onLogout(e) {
-   e.preventDefault();
+    e.preventDefault();
     await fetch('http://localhost:3030/users/logout', {
         method: 'GET',
         headers: {
@@ -30,7 +30,7 @@ async function onLogout(e) {
         },
     });
 
-    sessionStorage.clear(); 
+    sessionStorage.clear();
     window.location.href = './index.html';
 }
 
@@ -38,8 +38,8 @@ async function loadAllCatches() {
     const url = 'http://localhost:3030/data/catches';
     const response = await fetch(url);
     const data = await response.json();
- 
-  renderCatches(data);
+
+    renderCatches(data);
 }
 
 function renderCatches(data) {
@@ -48,66 +48,88 @@ function renderCatches(data) {
     const main = document.getElementById('main');
     main.replaceChildren();
     Object.values(data).forEach(c => {
-       const newCatchDIV = catchDIV.cloneNode(true);
+        const newCatchDIV = catchDIV.cloneNode(true);
         main.appendChild(newCatchDIV);
         const newAngler = newCatchDIV.getElementsByClassName('angler')[0];
         newAngler.value = `${c.angler}`;
-        
-        const newWeight= newCatchDIV.getElementsByClassName('weight')[0];
+
+        const newWeight = newCatchDIV.getElementsByClassName('weight')[0];
         newWeight.value = `${c.weight}`;
 
-        const newSpecies= newCatchDIV.getElementsByClassName('species')[0];
+        const newSpecies = newCatchDIV.getElementsByClassName('species')[0];
         newSpecies.value = `${c.species}`;
 
-        const newLocation= newCatchDIV.getElementsByClassName('location')[0];
+        const newLocation = newCatchDIV.getElementsByClassName('location')[0];
         newLocation.value = `${c.location}`;
-        
+
         const newBait = newCatchDIV.getElementsByClassName('bait')[0];
         newBait.value = `${c.bait}`;
-        
+
 
         const newCaptureTime = newCatchDIV.getElementsByClassName('captureTime')[0];
         newCaptureTime.value = `${c.captureTime}`;
 
     })
 
-    
-    
+
+
 }
 
-        
-async function addCatch(event) {
-    // TODO validations
-    const form = event.target.parentElement.parentElement;
 
+async function addCatch(event) {
+event.preventDefault();
+    const form = event.target.parentElement.parentElement;
     const formData = new FormData(form);
+
     const angler = formData.get('angler');
     const weight = formData.get('weight');
     const species = formData.get('species');
     const location = formData.get('location');
     const bait = formData.get('bait');
-    const captureTime = formData.get('captureTime');    
-    
-    const url = 'http://localhost:3030/data/catches';
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            'X-Authorization': sessionStorage.getItem('authToken')
-        },
-        body: JSON.stringify({
-            angler: angler,
-            weight: Number(weight),
-            species: species,
-            location: location,
-            bait: bait,
-            captureTime: Number(captureTime)
+    const captureTime = formData.get('captureTime');
+
+    try {
+        if (angler == '' || weight =='' || species == '' || location == '' || bait == '' || captureTime == '') {
+            return alert('Missing fields!');
+        }
+        if (Number(angler) || Number(species) || Number(location) || Number(bait)) {
+            return alert('Number is not allowed!');
+        }
+        if (isNaN(weight) || isNaN(captureTime)) {
+            return alert('Enter a number!');
+        }
+
+
+        const url = 'http://localhost:3030/data/catches';
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                'X-Authorization': sessionStorage.getItem('authToken')
+            },
+            body: JSON.stringify({
+                angler: angler,
+                weight: Number(weight),
+                species: species,
+                location: location,
+                bait: bait,
+                captureTime: Number(captureTime)
+            })
         })
-    })
+        
 
-    const data = await response.json();
+        const data = await response.json();
+        if (!response.ok || response.status != 200) {
+            throw new Error(data.message);
+        }
 
-    loadAllCatches()
-    return data;
+       
+        form.reset();
+        loadAllCatches()
+        return data;
 
-  }
+    } catch (e) {
+        alert(e.message);
+    }
+
+}
