04467719bfac7dbe21996c24f4c788575fa4bc44
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-19 19:40:04 +0200

    Last added: update - architecture refactored - navigation built with dependancy injection (context) - register functionality built - login functionality built

--- 09 JS Applications/03_ARCHITECTURE_AND_TESTING/SoftTerest/app.js
+++ 09 JS Applications/03_ARCHITECTURE_AND_TESTING/SoftTerest/app.js
@@ -12,37 +12,67 @@
 
 
 import { showHome } from "./src/views/home.js";
+import { showCatalog } from "./src/views/catalog.js";
+import { showLogin } from "./src/views/login.js";
+import { showRegister } from "./src/views/register.js";
+import { showCreate } from "./src/views/create.js";
+import { showDetails } from "./src/views/details.js";
 
 
 const main = document.getElementById("mainView");
+//the event is attached to the whole navigation bar
+//have to check where the target is
+document.querySelector("nav").addEventListener("click", onNavigate);
 
+// const registerView = document.getElementById("registerView");
+// const loginView = document.getElementById("loginView");
+// const dashboard = document.getElementById("dashboard-holder");
+// const detailsView = document.getElementById("detailsView");
+// const createView = document.getElementById("createView");
 
-const registerView = document.getElementById("registerView");
-const loginView = document.getElementById("loginView");
-const dashboard = document.getElementById("dashboard-holder");
-const detailsView = document.getElementById("detailsView");
-const createView = document.getElementById("createView");
-
-const defSection = document.getElementById("defSection").remove();
+document.getElementById("defSection").remove();
 
 const links = {
     "/": showHome,
-    "/catalog": dashboard,
-    "/login": loginView,
-    "/register": registerView,
-    "/details": detailsView,
-    "/create": createView
+    "/catalog": showCatalog,
+    "/login": showLogin,
+    "/register": showRegister,
+    "/details": showDetails,
+    "/create": showCreate
 }
 
 const context = {
     showSection
 }
 
-window.showHome = showHome
-window.context = context
+//TEST LINKS
+// window.showHome = showHome
+// window.context = context
 // showHome(context) -> in the browser
 
 
+
 function showSection(section) {
-    main.appendChild(section);
+    main.replaceChildren(section);
+}
+
+function onNavigate(e) {
+    // the <a> tags make rerender so event.preventDefault();
+    e.preventDefault();
+    let target = e.target;
+
+    if (target.tagName === "IMG") {
+        target = target.parentElement
+    }
+    if(target.tagName === "A") {
+        const url = new URL(target.href);
+        goTo(url.pathname); 
+    }
+}
+
+function goTo(name) {
+    const handler = links[name];
+    if(typeof(handler) === "function") {
+        handler(context)
+    }
 }
