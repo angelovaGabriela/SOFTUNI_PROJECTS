85e139d0b6c8913a7761544febff61c75a208116
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-28 13:01:38 +0200

    Last added: update -  catalog view render - details view render for each furniture

--- 09 JS Applications/05_BROWSER_ROUTING/Furniture/src/views/detailsView.js
+++ 09 JS Applications/05_BROWSER_ROUTING/Furniture/src/views/detailsView.js
@@ -1,3 +1,41 @@
-export async function detailsView() {
-    console.log("details")
+import { html } from "../../node_modules/lit-html/lit-html.js"
+import {getFurnitureDetails} from "../api/data.js"
+
+export async function detailsView(ctx) {
+    const furnitureId = ctx.params.id;
+    const details = await getFurnitureDetails(furnitureId);
+   ctx.render(detailsTemplate(details));
+}
+
+
+function detailsTemplate(data) {
+    const furnitureImgNameArr = data.img.split("/");
+    return html`
+      <div class="row space-top">
+            <div class="col-md-12">
+                <h1>Furniture Details</h1>
+            </div>
+        </div>
+        <div class="row space-top">
+            <div class="col-md-4">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <img src=${"/images/" + furnitureImgNameArr[furnitureImgNameArr.length - 1]}/>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <p>Make: <span>${data.make}</span></p>
+                <p>Model: <span>${data.model}</span></p>
+                <p>Year: <span>${Number(data.year)}</span></p>
+                <p>Description: <span>${data.description}</span></p>
+                <p>Price: <span>${data.price}</span></p>
+                <p>Material: <span>${data.material}</span></p>
+                <div>
+                    <a href=”#” class="btn btn-info">Edit</a>
+                    <a href=”#” class="btn btn-red">Delete</a>
+                </div>
+            </div>
+        </div>
+    `
 }
