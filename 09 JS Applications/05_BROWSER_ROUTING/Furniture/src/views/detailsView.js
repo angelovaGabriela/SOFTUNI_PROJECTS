dd7a9c275b1ae1b820184ecf0ba80affe0b3757d
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-07-28 15:15:48 +0200

    Last added: update - delete furniture functionality added

--- 09 JS Applications/05_BROWSER_ROUTING/Furniture/src/views/detailsView.js
+++ 09 JS Applications/05_BROWSER_ROUTING/Furniture/src/views/detailsView.js
@@ -1,16 +1,38 @@
-import { html } from "../../node_modules/lit-html/lit-html.js"
-import {getFurnitureDetails} from "../api/data.js"
 
+import { html } from "../../node_modules/lit-html/lit-html.js"
+import { getFurnitureDetails } from "../api/data.js"
+import { deleteFurnitureDetails } from "../api/data.js"
+
+let context = null;
 export async function detailsView(ctx) {
+    context = ctx;
     const furnitureId = ctx.params.id;
     const details = await getFurnitureDetails(furnitureId);
     const userData = JSON.parse(sessionStorage.getItem("userData"));
     const isOwner = userData._id === details._ownerId;
-   ctx.render(detailsTemplate(details, isOwner));
+   ctx.render(detailsTemplate(details, isOwner, daleteFurnitureById));
 }
 
 
-function detailsTemplate(data, isOwner) {
+async function daleteFurnitureById(event) {
+    event.preventDefault();
+    const id = event.target.dataset.id;
+    await deleteFurnitureDetails(id);
+    context.page.redirect("/")
+
+}
+
+function renderOwnerBtn(isOwner, daleteFurnitureById, id) {
+    return   isOwner ? html `
+
+    <a href=”#” class="btn btn-info">Edit</a>
+    <a @click=${daleteFurnitureById} data-id=${id} href=”javascript:void(0)” class="btn btn-red">Delete</a>
+    `
+    : ""
+
+}
+
+function detailsTemplate(data, isOwner, deleteFurnitureById) {
     const furnitureImgNameArr = data.img.split("/");
     return html`
       <div class="row space-top">
@@ -34,13 +56,8 @@ function detailsTemplate(data, isOwner) {
                 <p>Price: <span>${data.price} $</span></p>
                 <p>Material: <span>${data.material}</span></p>
                 <div>
-                    ${isOwner ? html `
+                  ${renderOwnerBtn(isOwner, deleteFurnitureById, data._id)}
 
-                    <a href=”#” class="btn btn-info">Edit</a>
-                    <a href=”#” class="btn btn-red">Delete</a>
-                    `
-                    : ""
-                    }
                 </div>
             </div>
         </div>
