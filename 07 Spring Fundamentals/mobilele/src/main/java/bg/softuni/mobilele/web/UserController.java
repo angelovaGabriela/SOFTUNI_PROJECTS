be46f7fee84b0ac5618aa38ae34f9e066912c546
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-10-10 19:38:09 +0200

    Last added: update error visualization on login

--- 07 Spring Fundamentals/mobilele/src/main/java/bg/softuni/mobilele/web/UserController.java
+++ 07 Spring Fundamentals/mobilele/src/main/java/bg/softuni/mobilele/web/UserController.java
@@ -1,9 +1,13 @@
 package bg.softuni.mobilele.web;
 
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -15,6 +19,16 @@ public class UserController {
     public String login() {
         return "auth-login";
     }
+
+  @PostMapping("/login-error")
+  public String onFailedLogin(
+          @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+         RedirectAttributes redirectAttributes
+  )  {
+        redirectAttributes.addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username);
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+        return "redirect:/users/login";
+  }
 
 
 
