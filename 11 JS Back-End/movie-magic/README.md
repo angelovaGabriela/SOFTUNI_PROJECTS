292eb426cb9655e6aab298205f9f88bd13c3b693
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2025-07-20 21:18:43 +0200

    add dynamic rendering of movies (partial used)

--- 11 JS Back-End/movie-magic/README.md
+++ 11 JS Back-End/movie-magic/README.md
@@ -8,4 +8,6 @@
 3. Setup handlebars
 4. Add resources
 5. Setup static route
-6. Add controller
+6. Render home page
+7. Add routes and controllers
+8. Extract partial template
--- 11 JS Back-End/movie-magic/src/controllers/home-controller.js
+++ 11 JS Back-End/movie-magic/src/controllers/home-controller.js
@@ -1,8 +1,32 @@
 import { Router } from 'express';
 const router = Router();
 
+const movies = [
+    {
+        id: 1,
+        title: 'Jungle Cuise',
+        imageUrl: '/img/jungle-cruise.jpeg',
+        genre: 'Adventure',
+        description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton'
+    },
+     {
+        id: 2,
+        title: 'The Little Mermaid',
+        imageUrl: '/img/the-little-mermaid.jpg',
+        genre: 'Fantasy',
+        description: 'The youngest of King Triton\'s daughters, Ariel is a beautiful and spirited young mermaid'
+    },
+     {
+        id: 3,
+        title: 'Home Alone',
+        imageUrl: '/img/home-alone.jpeg',
+        genre: 'Comedy',
+        description: 'It is Christmas time and the McCallister family is preparing for a vacation in Paris'
+    }
+];
+
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { movies });
 });
 
 router.get('/about', (req, res) => {
