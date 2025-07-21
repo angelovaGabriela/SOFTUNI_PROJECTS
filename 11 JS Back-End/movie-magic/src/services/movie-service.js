a62cb102c6f8f81ec91ef9a393c99a4512be7b00
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2025-07-21 18:23:47 +0200

    spliting code to 3 layer architecture

--- 11 JS Back-End/movie-magic/src/services/movie-service.js
+++ 11 JS Back-End/movie-magic/src/services/movie-service.js
@@ -1,29 +1,8 @@
-const movies = [
-    {
-        id: 1,
-        title: 'Jungle Cuise',
-        imageUrl: '/img/jungle-cruise.jpeg',
-        genre: 'Adventure',
-        description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton'
-    },
-     {
-        id: 2,
-        title: 'The Little Mermaid',
-        imageUrl: '/img/the-little-mermaid.jpg',
-        genre: 'Fantasy',
-        description: 'The youngest of King Triton\'s daughters, Ariel is a beautiful and spirited young mermaid'
-    },
-     {
-        id: 3,
-        title: 'Home Alone',
-        imageUrl: '/img/home-alone.jpeg',
-        genre: 'Comedy',
-        description: 'It is Christmas time and the McCallister family is preparing for a vacation in Paris'
-    }
-];
+import movieData from "../data/movie-data.js";
 
-const getAll = () => movies;
+const getAll = () => movieData.getMovies();
 
 export default {
     getAll,
 }
+
