cc890dcf28792da22bb25dc4d87be55fceca6f01
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2025-07-28 19:57:46 +0200

    add 404 page

--- 11 JS Back-End/movie-magic/src/routes.js
+++ 11 JS Back-End/movie-magic/src/routes.js
@@ -8,4 +8,8 @@ const router = Router();
 router.use(homeController);
 router.use('/movies', movieController);
 
+router.use((req, res) => {
+    res.status(404).render('404');
+});
+
 export default router;
