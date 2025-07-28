d4c22e450922f8d574f40bbc406e88a432486e3b
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2025-07-28 18:46:24 +0200

    add Details page

--- 11 JS Back-End/movie-magic/src/controllers/movie-controller.js
+++ 11 JS Back-End/movie-magic/src/controllers/movie-controller.js
@@ -13,5 +13,15 @@ router.post('/create', async (req, res) => {
   await movieService.create(movieData);
    
     res.redirect('/');
-})
+});
+
+router.get('/:movieId/details', async (req, res) => {
+
+    const movieId = req.params.movieId;
+
+    const movie = await movieService.getOne(movieId);
+
+    res.render('movies/details', { movie });
+});
+
 export default router;
