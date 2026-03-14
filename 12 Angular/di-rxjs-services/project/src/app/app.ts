4280835d493f2208a1a638dd284017f5bdb6b985
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-03-14 19:41:04 +0100

    services intro

---
+++ 12 Angular/di-rxjs-services/project/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Counter } from './components/counter/counter';
+@Component({
+  selector: 'app-root',
+  imports: [Counter],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  
+}
