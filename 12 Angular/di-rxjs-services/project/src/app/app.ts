c8f17a358fcc6f7dbd002736e7a08d86ab3af49f
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-03-15 19:08:28 +0100

    using of/from pipe(map/filter/tap)

--- 12 Angular/di-rxjs-services/project/src/app/app.ts
+++ 12 Angular/di-rxjs-services/project/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Counter } from './components/counter/counter';
+import { RxjsDemo  } from './components/rxjs-demo/rxjs-demo';
+
 @Component({
   selector: 'app-root',
-  imports: [Counter],
+  imports: [Counter, RxjsDemo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
