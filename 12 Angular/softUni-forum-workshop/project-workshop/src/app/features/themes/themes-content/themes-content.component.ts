e72c9b7a58b43f7e9be60501a4b4e7049a7544a3
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-04-08 22:20:43 +0200

    themes -> post a comment

--- 12 Angular/softUni-forum-workshop/project-workshop/src/app/features/themes/themes-content/themes-content.component.ts
+++ 12 Angular/softUni-forum-workshop/project-workshop/src/app/features/themes/themes-content/themes-content.component.ts
@@ -1,25 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../../core/services/api.service';
 import { Theme } from '../../../shared/interfaces/theme';
 import { Post } from '../../../shared/interfaces/post';
-
-
+import { AuthService } from '../../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-themes-content',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './themes-content.component.html',
   styleUrl: './themes-content.component.css',
 })
 export class ThemesContentComponent implements OnInit {
   private apiService = inject(ApiService);
   private route = inject(ActivatedRoute);
+  private authService = inject(AuthService);
 
   theme: Theme | null = null;
   posts: Post[] = [];
   commentText = '';
   themeId = '';
+
+  currentUsername = computed(() => this.authService.currentUser()?.username ?? 'Guest');
 
   ngOnInit(): void {
     this.themeId = this.route.snapshot.params['themeId'];
@@ -35,5 +38,10 @@ export class ThemesContentComponent implements OnInit {
     this.apiService.getLatestPosts().subscribe((posts) =>{
       this.posts = posts.filter((p: any) => p.themeId?._id === this.themeId);
     })
+  }
+
+  onPostComment(): void {
+    console.log('COMMENT TEXT:', this.commentText);
+    this.commentText = '';
   }
 }
