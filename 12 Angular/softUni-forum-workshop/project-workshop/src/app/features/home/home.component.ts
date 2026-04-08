291f7480ccdf582929afbef863ab88ac01b259e6
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-04-08 19:57:10 +0200

    home page

--- 12 Angular/softUni-forum-workshop/project-workshop/src/app/features/home/home.component.ts
+++ 12 Angular/softUni-forum-workshop/project-workshop/src/app/features/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private authService = inject(AuthService);
+
+  isloggedIn = this.authService.isLoggedIn();
+
+}
