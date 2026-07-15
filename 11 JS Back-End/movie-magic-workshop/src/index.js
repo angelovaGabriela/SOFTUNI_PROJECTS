a8ccc1abe30cdbe2607cfaf97876f18f8a860eba
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-07-15 19:26:46 +0200

    set up resources and debug mode

--- 11 JS Back-End/movie-magic-workshop/src/index.js
+++ 11 JS Back-End/movie-magic-workshop/src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello world!!');
 });
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000..'));
