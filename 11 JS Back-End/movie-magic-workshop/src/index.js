4318957d0309b20b32261bab16dbd5092ccdb0a0
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-07-15 19:14:53 +0200

    init server

--- 11 JS Back-End/movie-magic-workshop/src/index.js
+++ 11 JS Back-End/movie-magic-workshop/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello dev girl!")
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000..'));
