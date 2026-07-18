79ac9b69df5360f8062b6b974b66b9e648aefc3f
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2026-07-18 17:53:15 +0200

    add 'create movie' functionality with uuid for each movie

--- 11 JS Back-End/movie-magic-workshop/src/repositories/movieRepository.js
+++ 11 JS Back-End/movie-magic-workshop/src/repositories/movieRepository.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import  { v4 as uuid } from 'uuid';
 
 async function readDb(collection) {
     const content = await fs.readFile('./src/db.json', { encoding: 'utf-8' });
@@ -11,13 +12,26 @@ if (collection && !db.hasOwnProperty(collection)) {
     return collection ? db[collection] : db;
 }
 
+async function writeDb(db) {
+    const content = JSON.stringify(db, null, 2);
+     await fs.writeFile('./src/db.json', content, { encoding: 'utf-8' });
+}
 async function getAll() {
     const movies = await readDb('movies');
     return movies;
 }
 
+async function create(movieData) {
+    movieData.id = uuid();
+    const db = await readDb();
+
+    db.movies.push(movieData);
+
+    await writeDb(db);
+}
 const movieRepository = {
     getAll,
+    create,
 }
 
 export default movieRepository;
